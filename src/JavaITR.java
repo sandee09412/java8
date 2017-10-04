02a99ed561398c1fbd9b53043b6c6ebb4c2486ee
sandeep <sandeep.yadav@bcone.com>
2017-10-04 12:01:15 +0530

    Myadd

--- src/JavaITR.java
+++ src/JavaITR.java
@@ -4,7 +4,8 @@ import java.util.List;
 public class JavaITR {
     public static void main(String args[]) {
         List<Integer> list = Arrays.asList(1,3,5,7);
-       // list.forEach(i->System.out.println(i));
-        list.forEach(System.out::println);
+        list.forEach(i->System.out.println(i));
+        //list.forEach(System.out::println);
+
     }
 }
